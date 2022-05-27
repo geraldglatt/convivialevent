e2f57741db80cf24c6cb39852e6c2a2611712253
gerald glatt <geraldglatt89@gmail.com>
2022-05-27 12:33:37 +0200

    Modified UserFixtures

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -1,22 +1,31 @@
 <?php
 
 namespace App\DataFixtures;
+
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
 class AppFixtures extends Fixture
 {
+    private UserPasswordHasherInterface $hasher;
+
+    public function __construct(UserPasswordHasherInterface $hasher)
+   
+    {
+        $this->hasher = $hasher;
+    }
+
     public function load(ObjectManager $manager): void
     {
-       for($i =0;$i<10;$i++)
-       {
-           $user = new User();
-           $user->setUsername('username'.$i);
-           $user->setPassword('password'.$i);
-           $manager->persist($user);
-       }
+        $user = new User();
+        $user->setUsername('admin');
 
+        $password = $this->hasher->hashPassword($user, 'azerty');
+        $user->setPassword($password);
+       
+        $manager->persist($user);
         $manager->flush();
     }
 }
